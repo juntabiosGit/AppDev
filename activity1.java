8479782b1801e2532113749c90c8207441a0bb42
lemonjoes <jvalabado@paterostechnologicalcollege.edu.ph>
2025-02-18 22:28:38 +0800

    activity1.java  -  juffer victor alabado

--- activity1.java
+++ activity1.java
@@ -1,9 +1,6 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("Sandara L. Panis - BSIT 2D");
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juam Dela Cruz - BSIT 2C");
+        System.out.println("Juffer Victor Alabado - BSIT 2D");
+       
     }
 }
